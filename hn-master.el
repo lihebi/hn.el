3bf3ee01eed4a865439519a54f17207d056bd755
Hebi Li <lihebi.com@gmail.com>
2018-10-12 12:38:49 -0500

    initial design

---
+++ hn-master.el
@@ -0,0 +1,25 @@
+;;; hn-master.el --- A hacker news client. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2011 Free Software Foundation, Inc.
+
+;; Author: Hebi Li <lihebi.com@gmail.com>
+;; Version: 0.1
+;; Keywords: Scholar
+;; URL: https://github.com/lihebi/hn-master.el
+
+;;; Commentary:
+
+;; This package provides a minor mode to frobnicate and/or
+;; bifurcate any flanges you desire.  To activate it, just type
+
+;;; Code:
+
+;;;###autoload
+(defun hn-master ()
+  "Load hn-master interface."
+  (interactive))
+
+
+(provide 'hn-master)
+
+;;; hn-master.el ends here
